e8385b2d293977a335d097e120e9d675bbbfc53b
rertzer <rertzer@student.42.fr>
2024-08-18 13:51:09 +0200

    ex02 gray code done

---
+++ ex02_gray_code/gray_code.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gray_code.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rertzer <rertzer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 13:39:49 by rertzer           #+#    #+#             */
+/*   Updated: 2024/08/18 13:39:51 by rertzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GRAY_CODE_HPP
+# define GRAY_CODE_HPP
+
+# include <iostream>
+# include <string>
+# include <vector>
+
+using namespace std;
+
+uint32_t	gray_code(uint32_t n);
+
+#endif //! GRAY_CODE_HPP
---
+++ ex02_gray_code/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rertzer <rertzer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 11:37:48 by rertzer           #+#    #+#             */
+/*   Updated: 2024/08/18 11:37:49 by rertzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "gray_code.hpp"
+#include <sstream>
+#include <bitset>
+
+uint32_t	readNumber(char *str)
+{
+	uint32_t		n;
+	stringstream	ss;
+
+	ss.exceptions(ios::failbit);
+	
+	ss << str;
+	ss >> n;
+
+	return (n);
+}
+
+int	main(int argc, char **argv)
+{
+
+	if (argc != 2)
+	{
+		cerr << "adder require a number as arguments\n";
+		return (1);
+	}
+	
+	uint32_t	a;
+
+	try
+	{
+		a = readNumber(argv[1]);
+	}
+	catch (const exception& e)
+	{
+		cerr << "bad argument\n";
+		return (1);
+	}
+	
+	uint32_t	c = gray_code(a);
+	cout << "binary " << bitset<32>(a) << " (" << a << ")\n";
+	cout << "gray   " << bitset<32>(c) << " (" << c  << ")\n";
+
+	return (0);
+}
