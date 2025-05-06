335bd6386c60fea7832ecaed7894f46dcc898f2b
rertzer <rertzer@student.42.fr>
2025-05-06 11:34:50 +0200

    ex07 ok

--- ex07_sat/main.cpp
+++ ex07_sat/main.cpp
@@ -14,12 +14,38 @@
 
 int main() {
 	vector<string> formulas = {
-		"AB|", "AB&", "AA!&", "AA^", "AB>", "AB=", "ABCD||=", "AB|C&", "ABC|&", "ABCDEFG||||||",
-		//"ABCDEFGHIJKLMNOPQRSTUVWXYZ|||||||||||||||||||||||||"
+
+		"AB&",	   "AB|",	  "AB>",	"BA^",	  "BA^AB^=",	   "BA^AA^=", "AB=",
+		"ABAA||=", "AB|A&",	  "A!B|A&", "BB!|A&", "ABA|&",		   "ABA|&!",  "AAA",
+		"|A=",	   "A23",	  "A+A=B",	"A",	  "AX|",		   "AA!&",	  "AA^",
+		"AB>",	   "ABCD||=", "AB|C&",	"ABC|&",  "ABCDEFG||||||",
+		//"ABCDEFGHIJKLMNOPQRSTUVWXYZ|||||||||||||||||||||||||"};
+
 	};
 
 	for (auto form : formulas) {
-		cout << form << "\t" << boolalpha << sat(form) << "\n";
+		try {
+			cout << form << "\t" << boolalpha << sat(form) << "\n";
+		} catch (RPNException& e) {
+			cout << e.what() << endl;
+		}
 	}
 	return (0);
 }
+
+/*
+ * formula	-h						evaluation
+ * 10&		1 and 0						0
+ * 10|		1 or 0						1
+ * 11>		1 => 1						1
+ * 01^		0 xor 1						1
+ * 01^10^=	(0 xor 1) == (1 xor 0)		1
+ * 01^11^=	(0 xor 1) == (1 xor 1)		0
+ * 10=		1 == 0						0
+ * 1011||=	((1 or 1) or 0) == 1		1
+ * 10|1&	(1 or 0) and 1				1
+ * 1!0|1&	(not 1 or 0) and 1			0
+ * 00!|1&	(0 or not 0) and 1			1
+ * 101|&	1 and (1 or 0)				1
+ * 101|&!	1 and (1 or 0)				0
+ */
